2251ec136ebe1551853754c043514f6951f3d284
[Abhishek Rana <[rana@dal.ca>
2023-01-15 23:45:28 +0530

    created Nat gateway

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = "tf-user"
 }
 
@@ -16,5 +16,19 @@ module "vpc" {
   private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+# Create NAT Gateway
+module "nat-gateway" {
+  source                     = "../modules/nat-gateway"
+  public_subnet_az1_id       = module.vpc.mpublic_subnet_az1_id
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  internet_gateway           = module.vpc.internet_gateway
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+
 
 }
